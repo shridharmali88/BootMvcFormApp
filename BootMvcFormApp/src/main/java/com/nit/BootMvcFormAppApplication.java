0c7dd1bae3ecec948c9d112de96557005657c006
shridharmali88 <58371235+shridharmali88@users.noreply.github.com>
2020-02-16 11:39:43 +0530

    First Commit

---
+++ BootMvcFormApp/src/main/java/com/nit/BootMvcFormAppApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcFormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcFormAppApplication.class, args);
+	}
+
+}
